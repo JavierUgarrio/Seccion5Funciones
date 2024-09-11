24dbee53a250f025f5310ec3baeeced9afe53030
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-11 10:15:21 +0200

    return

--- Funciones/src/funciones/Funciones.java
+++ Funciones/src/funciones/Funciones.java
@@ -19,6 +19,10 @@ public class Funciones {
         //llamar Funcion
         saludar("Javier");
         sumar(1,2);
+        var sumarRr = sumarR(7,8);
+        System.out.println("la suma es "+  sumarRr);
+        System.out.println("la suma es "+ sumarR(21,2));
+        System.out.println(saludarR("Joselito"));
         
         //llamar Metodo
         Funciones s = new Funciones();
@@ -29,14 +33,23 @@ public class Funciones {
         Definir una funcion
         void, significa que va retonar
         los parametros son los datos que se envian a la funcion
+        return, retona un valor...ojo en la funcion si se pone el return hay que eliminar void y poner el tipo de dato que se va a retonar
     */
     static void saludar(String nombre){
         System.out.println("Hola " + nombre + " desde la funcion");
     }
     
+    static String saludarR(String nombre){
+        return "Hola " + nombre + " desde la funcion con return";
+    }
+    
     static void sumar(int num1, int num2){
         int suma = num1 + num2;
         System.out.println("la suma total es: " + suma);
+    }
+    
+    static int sumarR(int num1, int num2){
+        return num1+num2;
     }
     
     // Definir un metodo
